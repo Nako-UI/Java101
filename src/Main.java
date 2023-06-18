7b8db90f6e69d0879480a64af72dda0347e8f99a
okanbektas <okanbektas@yahoo.com>
2023-06-18 13:02:06 +0300

    Added main and write console Hello Emre and sum

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Emre");
+        int a,b;
+        a=5;
+        b=7;
+        System.out.println(a+b);
     }
 }
