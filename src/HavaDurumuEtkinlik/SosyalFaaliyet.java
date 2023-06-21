0d6a184b80c4552e6698bbf3979b3f7634f1f02d
okanbektas <okanbektas@yahoo.com>
2023-06-21 13:18:30 +0300

    new branch 9

---
+++ src/HavaDurumuEtkinlik/SosyalFaaliyet.java
@@ -0,0 +1,31 @@
+package HavaDurumuEtkinlik;
+
+import java.util.Scanner;
+
+public class SosyalFaaliyet {
+    public static void main(String[] args) {
+        int heat;
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Lütfen hava sıcaklığını giriniz : ");
+        heat = input.nextInt();
+
+        if (heat<5){
+            System.out.println("Kayak yapabilirsiniz.");
+        }
+
+        if (heat>=5 && heat<=15){
+            System.out.println("Sinemaya gidebilirsiniz.");
+        } else if (heat==5){
+            System.out.println("Kayak yapabilirsiniz.");
+        }
+        if (heat>=15 && heat<=25){
+            System.out.println("Piknik yapabilirsiniz.");
+        } else if (heat==25) {
+            System.out.println("Yüzmeye gidebilirsiniz.");
+        }
+        if (heat>25){
+            System.out.println("Yüzmeye gidebilirsiniz.");
+        }
+    }
+}
